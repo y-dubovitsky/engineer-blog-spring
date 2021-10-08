4977b4b5429574c4cced79708546b88d4d2d4280
user <y.dubovitsky@gmail.com>
2021-10-08 17:31:54 +0300

    Init commit

---
+++ src/main/java/ru/ydubovitsky/engineerblog/EngineerBlogApplication.java
@@ -0,0 +1,13 @@
+package ru.ydubovitsky.engineerblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EngineerBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EngineerBlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/ydubovitsky/engineerblog/EngineerBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ydubovitsky.engineerblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EngineerBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
