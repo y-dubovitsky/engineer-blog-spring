11fee8b6faf224957c6bf96550a9fb0eaedda942
user <y.dubovitsky@gmail.com>
2022-04-28 10:07:35 +0300

    The 'Comment' and the 'main' microservices registered as Eureka clients.

--- comment/src/main/java/ru/ydubovitsky/comment/CommentApplication.java
+++ comment/src/main/java/ru/ydubovitsky/comment/CommentApplication.java
@@ -2,7 +2,9 @@ package ru.ydubovitsky.comment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class CommentApplication {
     public static void main(String[] args) {
--- main/src/main/java/ru/ydubovitsky/engineerBlog/EngineerBlogApplication.java
+++ main/src/main/java/ru/ydubovitsky/engineerBlog/EngineerBlogApplication.java
@@ -2,8 +2,10 @@ package ru.ydubovitsky.engineerBlog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class EngineerBlogApplication {
 
 	public static void main(String[] args) {
